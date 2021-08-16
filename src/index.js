ed62a37af9d9a99c1801e7fc9c7247d235786e91
B-12 <aabduvakhobov02@gmail.com>
2021-08-16 23:59:31 +0500

    Created actions and reducer files, implemented bindActionCreators from redux

--- src/index.js
+++ src/index.js
@@ -1,37 +1,15 @@
-import { createStore } from "redux";
-
-const reducer = (state = 0, action) => {
-  switch (action.type) {
-    case "INC":
-      return state + 1;
-    case "DEC":
-      return state - 1;
-    case "RND":
-      return (state = 0);
-    default:
-      return state;
-  }
-};
+import { createStore, bindActionCreators } from "redux";
+import reducer from "./reducer";
+import * as actions from "./actions";
 
 const store = createStore(reducer);
+const { dispatch } = store;
 
-const inc = () => ({ type: "INC" });
-const dec = () => ({ type: "DEC" });
-const rnd = (value) => ({ type: "RND" });
-// const rnd = (value) => ({ type: "RND", value });
+const { inc, dec, ref } = bindActionCreators(actions, dispatch);
 
-document.querySelector("#inc").addEventListener("click", () => {
-  store.dispatch(inc());
-});
-document.querySelector("#dec").addEventListener("click", () => {
-  store.dispatch(dec());
-});
-document.querySelector("#rnd").addEventListener("click", () => {
-  store.dispatch(rnd());
-});
-// document.querySelector("#rnd").addEventListener("click", () => {
-//     store.dispatch(rnd(Math.floor(Math.random() * 10)));
-//   });
+document.querySelector("#inc").addEventListener("click", inc);
+document.querySelector("#dec").addEventListener("click", dec);
+document.querySelector("#ref").addEventListener("click", ref);
 
 const update = () => {
   document.querySelector("#counter").textContent = store.getState();
